48036dea62307b5b239d7c68915e02d0f259b235
nono38 <74985866+nono38@users.noreply.github.com>
2021-02-11 10:48:01 +0100

    Mise à jour pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,12 @@
-let engle = 0
 input.onButtonPressed(Button.A, function () {
+    if (gjmyy == 0) {
+        basic.showString("VA DANS LE SALON AU NORD OUEST")
+        gjmyy = 1
+    } else {
+        basic.showString("VA DANS LA CHAMBRE EST AU SE")
+    }
+})
+input.onButtonPressed(Button.B, function () {
     engle = input.compassHeading()
     if (engle > 337.5 && engle < 359) {
         basic.showString("N")
@@ -13,10 +20,27 @@ input.onButtonPressed(Button.A, function () {
         if (engle > 0 && engle < 22.5) {
             basic.showString("N")
         } else {
-        	
+            if (engle > 157.5 && engle < 202.5) {
+                basic.showString("S")
+            } else {
+                if (engle > 202.5 && engle < 247.5) {
+                    basic.showString("SO")
+                } else {
+                    if (engle > 247.5 && engle < 292.5) {
+                        basic.showString("O")
+                    } else {
+                        if (engle > 292.5 && engle < 337.5) {
+                            basic.showString("NO")
+                        }
+                    }
+                }
+            }
         }
     }
 })
+let engle = 0
+let gjmyy = 0
+gjmyy = 0
 basic.forever(function () {
 	
 })
